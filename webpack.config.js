4b7457ec893c9c2263471f78c9be89981b85a802
VladDev26 <volodimirb23@gmail.com>
2017-02-24 12:11:35 +0200

    a

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,81 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
-var production = new webpack.DefinePlugin({
-  'process.env': {
-    NODE_ENV: JSON.stringify('production')
-  }
+const production = new webpack.DefinePlugin({
+  'process.env': { NODE_ENV: JSON.stringify('production') }
 });
 
 
 module.exports = {
   entry: path.resolve(__dirname + '/src/index.js'),
   output: {
-    path: path.join(__dirname, "assets"),
-    filename: 'bundle.js',
-    publicPath: '/assets/'
+	path: path.join(__dirname, "assets"),
+	filename: 'bundle.js',
+	publicPath: '/assets/'
   },
   
   module: {
-    rules: [
-      { 
-        test: /\.js$/, 
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        options: { presets: ['es2015', 'react', 'stage-2'] }
-      },
-      
-      { 
-        test: /\.scss$/i, 
-        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      },
+	rules: [
+	  // { 
+	  //   test: /\.js$/, 
+	  //   exclude: /(node_modules)/,
+	  //   loader: 'babel-loader',
+	  //   options: { presets: ['es2015', 'react', 'stage-2'] }
+	  // },
+	  
 
-      { 
-        test: /.*\.(ttf|eot|woff2?|svg)(\?.*$|$)/i,
-        loader: "file-loader?name=fonts/[name]/[name].[ext]"
-      },
-      { 
-        test: /\.html$/i,
-        loader: "html-loader"
-      },
-
-      { 
-        test: /.*\.(gif|png|jpg)$/i, 
-        loader: "file-loader?name=img/[name].[ext]"
+	  {
+        test: /\.scss/,
+        use: ExtractTextPlugin.extract([
+        	'css-loader',
+        	'sass-loader',
+        	{
+	            loader: 'postcss-loader',
+	            options: {
+	              plugins: function () {
+	                return [
+	                  require('autoprefixer')
+	                ];
+	              }
+	            }
+	        }
+        ])
       }
-    ],
+	  // {
+   //      test: /\.css/,
+   //      use: [
+   //        'css-loader',
+   //        {
+   //          loader: 'postcss-loader',
+   //          options: {
+   //            plugins: function () {
+   //              return [
+   //                require('autoprefixer')
+   //              ];
+   //            }
+   //          }
+   //        }
+   //      ]
+   //    }
+
+	  // { 
+	  //   test: /.*\.(ttf|eot|woff2?|svg)(\?.*$|$)/i,
+	  //   loader: "file-loader?name=fonts/[name]/[name].[ext]"
+	  // },
+	  // { 
+	  //   test: /\.html$/i,
+	  //   loader: "html-loader"
+	  // },
+
+	  // { 
+	  //   test: /.*\.(gif|png|jpg)$/i, 
+	  //   loader: "file-loader?name=img/[name].[ext]"
+	  // }
+	],
   },
   plugins: [
-    // production,
-    // new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin('styles/bundle.min.css')
+	new ExtractTextPlugin('styles/bundle.css')
   ]
 };
